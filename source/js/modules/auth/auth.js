db6cb56404cca4ffaf701f5ddbb17aa62ecff420
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-05-03 22:45:03 +0300

    fixies modal in calculator and options js

--- source/js/modules/auth/auth.js
+++ source/js/modules/auth/auth.js
@@ -4,9 +4,11 @@ const registrationForm = document.querySelector('.registration-modal');
 const registrationButton = document.querySelector('.header-navigation__registration-button');
 const usersProfile = document.querySelector('.user-navigation-header');
 const imageUploadButton = registrationForm.querySelector('.registration-modal__input-image-upload');
+function checkLocalStorage() {
+  if (localStorage.getItem('userProfile') !== null) {
 
-function checkLocalStorage () {
-  if(localStorage.getItem('userProfile') !== null) {
+    if (registrationForm !== null) {
+
       registrationButton.classList.add('hidden');
       usersProfile.classList.remove('hidden');
       const profile = localStorage.getItem('userProfile');
@@ -14,18 +16,24 @@ function checkLocalStorage () {
       usersProfile.querySelector('.user-profile__img').src = userProfile.avatarUrl;
       usersProfile.querySelector('.user-navigation-header__title').textContent = userProfile.name;
       registrationButton.removeEventListener('click', onRegistrationButton);
+    }
   }
 }
 
-function onRegistrationButton () {
-  registrationForm.classList.remove('hidden');
-  imageUploadButton.addEventListener('change', uploadImg);
-  registrationForm.addEventListener('submit', onSubmitForm);
+function onRegistrationButton() {
+  if (registrationForm !== null) {
+    registrationForm.classList.remove('hidden');
+    imageUploadButton.addEventListener('change', uploadImg);
+    registrationForm.addEventListener('submit', onSubmitForm);
+  }
 }
 
-function onWindowLoad () {
-  registrationButton.addEventListener('click', onRegistrationButton);
-  checkLocalStorage();
+function onWindowLoad() {
+  if (registrationForm !== null) {
+    registrationButton.addEventListener('click', onRegistrationButton);
+    checkLocalStorage();
+  }
 }
+
 
 export { onWindowLoad, checkLocalStorage }
