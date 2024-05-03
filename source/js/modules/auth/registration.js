569f9246df1b272ab4bf843825bc413e24aee786
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-05-03 22:48:04 +0300

    fixies js

--- source/js/modules/auth/registration.js
+++ source/js/modules/auth/registration.js
@@ -8,38 +8,43 @@ const MAX_IMAGE_SIZE = 2000000;
 
 const reader = new FileReader();
 
-function uploadImg () {
-  const file = imageUploadButton.files[0];
-  if(file.size > MAX_IMAGE_SIZE) {
-    registrationForm.querySelector('.registration-modal__error-mesage').textContent = 'Размер файла не должен привышать 2 Мб';
-    registrationForm.querySelector('.registration-modal__error-mesage').classList.remove('hidden');
-  } else {
-    registrationForm.querySelector('.registration-modal__error-mesage').classList.add('hidden');
-    reader.addEventListener('load', onImageUpload)
-    reader.readAsDataURL(file);
+function uploadImg() {
+  if (registrationForm !== null) {
+    const file = imageUploadButton.files[0];
+    if (file.size > MAX_IMAGE_SIZE) {
+      registrationForm.querySelector('.registration-modal__error-mesage').textContent = 'Размер файла не должен привышать 2 Мб';
+      registrationForm.querySelector('.registration-modal__error-mesage').classList.remove('hidden');
+    } else {
+      registrationForm.querySelector('.registration-modal__error-mesage').classList.add('hidden');
+      reader.addEventListener('load', onImageUpload)
+      reader.readAsDataURL(file);
+    }
   }
 }
 
-function onImageUpload () {
-  usersImagePreviews.src = reader.result;
+function onImageUpload() {
+  if (registrationForm !== null) {
+    usersImagePreviews.src = reader.result;
+  }
 }
 
 
-function onSubmitForm (evt) {
+function onSubmitForm(evt) {
+  if (registrationForm !== null) {
     evt.preventDefault();
     const userName = registrationForm.querySelector('#name').value;
     const userEmail = registrationForm.querySelector('#email').value;
     const userAvatarUrl = usersImagePreviews.src;
     const userProfile = {
-        name: userName,
-        email: userEmail,
-        avatarUrl: userAvatarUrl
+      name: userName,
+      email: userEmail,
+      avatarUrl: userAvatarUrl
     }
     localStorage.setItem('userProfile', JSON.stringify(userProfile))
     registrationForm.classList.add('hidden');
     imageUploadButton.removeEventListener('change', uploadImg);
     registrationForm.removeEventListener('submit', onSubmitForm);
     checkLocalStorage();
+  }
 }
-
 export { uploadImg, onSubmitForm }
