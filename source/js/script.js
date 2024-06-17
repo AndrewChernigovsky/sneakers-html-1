818bb43a7ed686478dce70e38efa66d8de642bfb
xfir664 <xfir664@yandex.ru>
2024-06-17 21:34:34 +0300

    header

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 import "./modules/calculator.js";
 // import { data } from "./data/data.js";
 import { onWindowLoad } from "./modules/auth/auth";
+import { burgerMenuToggle } from "./modules/burger-menu.js"
 
 window.addEventListener('DOMContentLoaded', onWindowLoad);
+burgerMenuToggle();
+
