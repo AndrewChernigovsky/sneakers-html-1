e61ae69275dddbb07aaf95c7f991c4454d17ea6d
Andrew Izosimov <chernigovsky108@gmail.com>
2024-06-22 20:31:22 +0300

    #IT-65 fix: fixies

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,38 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const slides = document.querySelectorAll('.slider__slide');
-  
-  // Функция для получения следующего слайда по data-slide
-  const getNextSlide = (currentSlide) => {
-    const currentSlideNumber = parseInt(currentSlide.dataset.slide);
-    const nextSlideNumber = (currentSlideNumber % slides.length) + 1;
-    return document.querySelector(`.slider__slide[data-slide="${nextSlideNumber}"]`);
-  };
+// Найти все слайды
+const slides = document.querySelectorAll('.slider__slide');
+let currentSlide = 0;
 
-  let currentSlide = slides[0];
-  currentSlide.classList.add('slider__active');
-
-  const changeSlide = () => {
-    currentSlide.classList.remove('slider__active');
-    currentSlide = getNextSlide(currentSlide);
-    currentSlide.classList.add('slider__active');
-  };
-
-  setInterval(changeSlide, 6000); // Переключение каждые 6 секунд
-});
-
-/* document.addEventListener('DOMContentLoaded', () => {
-  // Найти все слайды
-  const slides = document.querySelectorAll('.slider__slide');
-  let currentSlide = 0;
-
-  // Функция для переключения слайдов
-  const changeSlide = () => {
-    slides[currentSlide].classList.remove('slider__active');   //удаляет класс
-    currentSlide = (currentSlide + 1) % slides.length; // Рассчитать следующий слайд
-    slides[currentSlide].classList.add('slider__active'); // Добавить класс 'slider__active' следующему слайду
-  };
-
-  slides[0].classList.add('slider__active'); // Установить класс 'slider__active' первому слайду при загрузке страницы
-
-  setInterval(changeSlide, 6000); // Каждые 5 секунд
-}); */
+// Функция для переключения слайдов
+export const changeSlide = () => {
+  slides[currentSlide].classList.remove('slider__active');   //удаляет класс
+  currentSlide = (currentSlide + 1) % slides.length; // Рассчитать следующий слайд
+  slides[currentSlide].classList.add('slider__active'); // Добавить класс 'slider__active' следующему слайду
+};
--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ import "./modules/calculator.js";
 import { DATA } from "./data/data.js";
 import { onWindowLoad } from "./modules/auth/auth";
 import { burgerMenuToggle } from "./modules/burger-menu.js"
+import { changeSlide } from "./modules/slider.js";
 
-window.addEventListener('DOMContentLoaded', onWindowLoad);
+window.addEventListener('DOMContentLoaded', onWindowLoad, setInterval(changeSlide, 2000));
 
-import "./modules/slider.js";
