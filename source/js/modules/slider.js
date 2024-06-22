616122155d7f4fef4e787fcf9361f85ed4016e8a
Tatiana B <tbeketova04@gmail.com>
2024-06-22 16:57:20 +0300

    #IT-65 added style and script for slider

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const slides = document.querySelectorAll('.slider__slide');
+  
+  // Функция для получения следующего слайда по data-slide
+  const getNextSlide = (currentSlide) => {
+    const currentSlideNumber = parseInt(currentSlide.dataset.slide);
+    const nextSlideNumber = (currentSlideNumber % slides.length) + 1;
+    return document.querySelector(`.slider__slide[data-slide="${nextSlideNumber}"]`);
+  };
+
+  let currentSlide = slides[0];
+  currentSlide.classList.add('slider__active');
+
+  const changeSlide = () => {
+    currentSlide.classList.remove('slider__active');
+    currentSlide = getNextSlide(currentSlide);
+    currentSlide.classList.add('slider__active');
+  };
+
+  setInterval(changeSlide, 6000); // Переключение каждые 6 секунд
+});
+
+/* document.addEventListener('DOMContentLoaded', () => {
   // Найти все слайды
   const slides = document.querySelectorAll('.slider__slide');
   let currentSlide = 0;
@@ -13,4 +35,4 @@ document.addEventListener('DOMContentLoaded', () => {
   slides[0].classList.add('slider__active'); // Установить класс 'slider__active' первому слайду при загрузке страницы
 
   setInterval(changeSlide, 6000); // Каждые 5 секунд
-});
+}); */
