96d5ff77c69e120f707f03ed711d5e92f2a5f413
Andrew Izosimov <chernigovsky108@gmail.com>
2024-06-29 19:40:46 +0300

    #IT-65 fix: script and change seconds

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
 import "./modules/calculator.js";
-import { DATA } from "./data/data.js";
+// import { DATA } from "./data/data.js";
 import { onWindowLoad } from "./modules/auth/auth";
 import { burgerMenuToggle } from "./modules/burger-menu.js"
 import { changeSlide } from "./modules/slider.js";
 
-window.addEventListener('DOMContentLoaded', onWindowLoad, setInterval(changeSlide, 2000));
+window.addEventListener('DOMContentLoaded', onWindowLoad, setInterval(changeSlide, 6000), burgerMenuToggle());
 
