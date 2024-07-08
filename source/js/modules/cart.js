4f606722fa126a483e5b638fece99f933b0f48e7
Andrew Izosimov <chernigovsky108@gmail.com>
2024-07-08 13:36:23 +0300

    fixes

---
+++ source/js/modules/cart.js
@@ -0,0 +1,11 @@
+// Шапка в корзине с фильтрами
+
+// p.cart-buttons__cost
+// 			span.total-count 6 общее количество товаров
+// 			span товаров на сумму:
+// 			span.total-price 142 794 итоговая цена всех товаров
+// 			span.currency ₽
+
+
+
+
